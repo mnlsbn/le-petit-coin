69414bf6158020f5aa323f3720806367275c3113
romain despature <rdespature@gmail.com>
2017-05-22 22:43:06 +0200

    put has_many attributes on seperated lines to correct a bug linked to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_many :reviews, :bookings, :wcs  #faut il mettre une relation has_many wcs through bookings ?
+  has_many :reviews
+  has_many :bookings  #faut il mettre une relation has_many wcs through bookings ?
   has_one :profile
 end
