6a1eec25e389600c0de0eda69767642b508684dc
romain despature <rdespature@gmail.com>
2017-05-22 22:57:09 +0200

    forgot to put again has_many wcs after trying to fix the bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :reviews
-  has_many :bookings  #faut il mettre une relation has_many wcs through bookings ?
+  has_many :reviews  #attention, il faut laisser sur plusieurs lignes, sinon bug
+  has_many :bookings
+  has_many :wcs #faut il mettre une relation has_many wcs through bookings ?
   has_one :profile
 end
