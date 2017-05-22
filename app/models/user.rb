75a9064f584b3ae0c7d9020c9617093fd64228c4
roms182 <rdespature@gmail.Com>
2017-05-22 23:05:15 +0200

    Revert "Add seeds for user + changes on has_many (put on different lines) to prevent a bug"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_many :reviews  #attention, il faut laisser sur plusieurs lignes, sinon bug
-  has_many :bookings
-  has_many :wcs #faut il mettre une relation has_many wcs through bookings ?
+  has_many :reviews, :bookings, :wcs  #faut il mettre une relation has_many wcs through bookings ?
   has_one :profile
 end
--- app/models/wc.rb
+++ app/models/wc.rb
@@ -1,5 +1,4 @@
 class Wc < ApplicationRecord
   belongs_to :user
-  has_many :reviews
-  has_many :bookings
+  has_many :reviews, :bookings
 end
