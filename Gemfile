2ab535e691d15f5b8b328d0b2ba8450654e8ce12
Vincent Textoris <vincent.textoris@gmail.com>
2017-05-23 13:43:23 +0200

    delete old migrations (martin)

--- Gemfile
+++ Gemfile
@@ -16,10 +16,13 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 
+#sign in & login gem
+gem 'devise'
+
 group :development, :test do
   gem 'binding_of_caller'
   gem 'better_errors'
-  
+
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'spring'
--- db/schema.rb
+++ db/schema.rb
@@ -10,63 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522163049) do
+ActiveRecord::Schema.define(version: 20170523102653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bookings", force: :cascade do |t|
-    t.boolean  "status"
-    t.integer  "user_id"
-    t.integer  "wc_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
-    t.index ["wc_id"], name: "index_bookings_on_wc_id", using: :btree
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "phone_number"
-    t.string   "picture"
-    t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text     "description"
-    t.integer  "rating"
-    t.integer  "wc_id"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
-    t.index ["wc_id"], name: "index_reviews_on_wc_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "wcs", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.string   "address"
-    t.boolean  "availability"
-    t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["user_id"], name: "index_wcs_on_user_id", using: :btree
-  end
-
-  add_foreign_key "bookings", "users"
-  add_foreign_key "bookings", "wcs"
-  add_foreign_key "profiles", "users"
-  add_foreign_key "reviews", "users"
-  add_foreign_key "reviews", "wcs"
-  add_foreign_key "wcs", "users"
 end
