fbc6228327441652f5237f1762c07e081165512a
manuel sabine <manuel.sabine@gmail.com>
2017-05-24 14:51:45 +0200

    show profile update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :profiles, except: [:index]
   resources :wcs, only: [:index, :new, :create]
 
+  resource :profile, controller: :profile
   devise_for :users
-  root to: 'pages#home'
 
   resources :wcs do
     resources :bookings
   end
+
+  root to: 'pages#home'
+
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523151249) do
+ActiveRecord::Schema.define(version: 20170524122751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20170523151249) do
     t.string   "last_name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -79,5 +81,6 @@ ActiveRecord::Schema.define(version: 20170523151249) do
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "wcs"
+  add_foreign_key "profiles", "users"
   add_foreign_key "wcs", "users"
 end
