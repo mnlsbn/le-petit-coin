76ad70e435fbf2b394cafc4349e095cc8227215f
manuel sabine <manuel.sabine@gmail.com>
2017-05-24 16:17:37 +0200

    geocode wip

--- app/controllers/wcs_controller.rb
+++ app/controllers/wcs_controller.rb
@@ -1,10 +1,19 @@
 class WcsController < ApplicationController
   def index
     @wcs = Wc.all
+
+    @wcs = Wc.where.not(latitude: nil, longitude: nil)
+
+      @hash = Gmaps4rails.build_markers(@wcs) do |wc, marker|
+        marker.lat wc.latitude
+        marker.lng wc.longitude
+        marker.infowindow render_to_string(partial: "/wcs/map_box", locals: { wc: wc })
+      end
   end
 
   def show
     @wc = Wc.find(params[:id])
+    @wc_coordinates = { lat: @wc.latitude, lng: @wc.longitude }
   end
 
   def new
