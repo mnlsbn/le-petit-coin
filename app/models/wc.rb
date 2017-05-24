d934e2e0ff27ba528c09f602b1ca53d7200db430
romain despature <rdespature@gmail.com>
2017-05-24 14:30:57 +0200

    index and new views improved

--- app/models/wc.rb
+++ app/models/wc.rb
@@ -4,7 +4,7 @@ class Wc < ApplicationRecord
   #has_many :reviews
   validates :name, presence: { message: "please give a name to your toilets" }
   validates :address, presence: { message: "please indicate the address of your toilets" }
-  validates :availability, presence: { message: "you need to indicate whether your toilets are available or not (true or false)" }
-  #validates :name, uniqueness: { scope: :user_id }
+  # bug avec Form WC New / validates :availability, presence: { message: "you need to indicate whether your toilets are available or not (true or false)" }
   validates_uniqueness_of :name, :scope => [:user_id]
 end
+
