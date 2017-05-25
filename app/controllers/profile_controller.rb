a4e4859ebe495983351b1361509b4cfe7942f4b8
manuel sabine <manuel.sabine@gmail.com>
2017-05-25 17:48:35 +0200

    edit profile wip

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,5 @@
 class ProfileController < ApplicationController
+  before_action :set_profile, only: [:update, :destroy]
   def new
     @profile = Profile.new
   end
@@ -20,14 +21,22 @@ class ProfileController < ApplicationController
   end
 
   def update
+    @profile.update(profile_params)
+    redirect_to profile_path(@profile)
   end
 
   def destroy
+    @profile.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :picture, :phone_number, :photo, :photo_cache)
+    params.require(:profile).permit(:first_name, :last_name, :phone_number, :photo, :photo_cache)
+  end
+
+  def set_profile
+    @profile = Profile.find(params[:id])
   end
 end
