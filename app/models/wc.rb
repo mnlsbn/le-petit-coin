6d7326fe3be40b3d284faf2f5025b3eae7efcd19
manuel sabine <manuel.sabine@gmail.com>
2017-05-25 14:50:50 +0200

    Adds attachinary #63

--- app/models/wc.rb
+++ app/models/wc.rb
@@ -2,6 +2,8 @@ class Wc < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
+  has_attachment :photo
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
   #has_many :reviews
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  as :user do
+  get 'profile/new', :to => 'devise/sessions#create', :as => :user_root
+  end
+
   resources :wcs do
     resources :bookings
   end
