99d792657ef2bbab2d1ed15210f47b17640f7526
Juliette Scheidecker <juscheidecker@gmail.com>
2017-05-25 15:16:04 +0200

    #64 review method controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,31 @@
 class ReviewsController < ApplicationController
-  def index
-  end
+  # def index
+  #   @reviews = Review.all
+  # end
 
   def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    if @review.save
+      redirect_to  wc_bookings_path(@wc)
+    else
+      render :new
+    end
   end
 
-  def show
+  # def show
+  #   @review = Review.find(params[:id])
+  # end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
   end
+
 end
